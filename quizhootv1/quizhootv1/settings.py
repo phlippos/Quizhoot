68e1ec8f4122408678acd763380d2794e7f10412
Cihangir <123465447+phlippos@users.noreply.github.com>
2024-12-17 10:03:33 +0300

    quiz-debug

--- quizhootv1/quizhootv1/settings.py
+++ quizhootv1/quizhootv1/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'quizhoot',  
         'USER': 'root', 
-        'PASSWORD': 'Memo2003', 
+        'PASSWORD': 'cihangir', 
         'HOST': 'localhost',  
         'PORT': '3306',  
     }
