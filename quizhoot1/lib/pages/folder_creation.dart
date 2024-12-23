37674a25946abd964b77689488560056e20bcaf4
AbdullahOztoprak <e210503006@stud.tau.edu.tr>
2024-12-23 18:24:01 +0300

    Cards and folder save button💕

--- quizhoot1/lib/pages/folder_creation.dart
+++ quizhoot1/lib/pages/folder_creation.dart
@@ -24,7 +24,7 @@ class _CreateFolderPageState extends State<CreateFolderPage> {
 
   void _showFolderDialog() {
     TextEditingController folderNameController =
-    TextEditingController(); // Controller for the folder name input
+        TextEditingController(); // Controller for the folder name input
 
     showDialog(
       context: context,
@@ -54,7 +54,7 @@ class _CreateFolderPageState extends State<CreateFolderPage> {
                       return CheckboxListTile(
                         title: Text(availableSets[index]), // Displays set name
                         value: setSelections[
-                        index], // Checks if the set is selected
+                            index], // Checks if the set is selected
                         onChanged: (bool? value) {
                           setState(() {
                             setSelections[index] =
@@ -91,6 +91,36 @@ class _CreateFolderPageState extends State<CreateFolderPage> {
     );
   }
 
+  void _saveFolder() {
+    // Save functionality
+    List<String> selectedSets = setSelections
+        .asMap()
+        .entries
+        .where((entry) => entry.value)
+        .map((entry) => availableSets[entry.key])
+        .toList();
+
+    if (folderName.isNotEmpty && selectedSets.isNotEmpty) {
+      // Show a snackbar with folder name and selected sets
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Folder "$folderName" with sets ${selectedSets.join(', ')} saved!'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    } else {
+      // Show an error snackbar if folder name or selected sets are missing
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content:
+              Text('Please provide a valid name and select at least one set.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,6 +128,12 @@ class _CreateFolderPageState extends State<CreateFolderPage> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF3A1078), // AppBar background color
         title: const Text('Folder Creation'), // AppBar title
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save), // Save icon
+            onPressed: _saveFolder, // Trigger save functionality on press
+          ),
+        ],
       ),
       body: Center(
         child: Column(
